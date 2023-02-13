0a93c42394c8a371cc2fc540745bb4057d519052
jgerstmayr <johannes.gerstmayr@uibk.ac.at>
2023-02-13 17:41:11 +0000

    Deploying to gh-pages from @ jgerstmayr/EXUDYN@6a83e8b455278736df3e87c2b746a8c025f9aece 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '1.5.97.dev1',
-    LANGUAGE: 'en',
+    LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
