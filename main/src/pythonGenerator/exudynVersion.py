b428cda78ef745e2092e7d202e72d84881bf4667
jgerstmayr <johannes.gerstmayr@uibk.ac.at>
2025-11-11 15:14:57 +0100

    fix version on readthedocs

--- main/src/pythonGenerator/exudynVersion.py
+++ main/src/pythonGenerator/exudynVersion.py
@@ -1,6 +1,9 @@
 #read Python version from theDoc file and store in string
-
 version = 'unknown'
+
+# import os
+# print('exudynVersion.py: CWD='+os.getcwd())
+
 #depending on from where we search, we import version ... (used from code generators and from setup.py!)
 try:
     with open('../../../docs/theDoc/version.txt', 'r') as file:
@@ -14,7 +17,11 @@ except FileNotFoundError:
             with open('../docs/theDoc/version.txt', 'r') as file:
                 version = file.read().strip()
         except FileNotFoundError:
-            print(f"Error: The file 'version.txt' could not be found; check paths to docs/theDoc/")
+            try:
+                with open('docs/theDoc/version.txt', 'r') as file:
+                    version = file.read().strip()
+            except FileNotFoundError:
+                print(f"Error: The file 'version.txt' could not be found; check paths to docs/theDoc/")
 except Exception as e:
     print(f"An error occurred: {e}")
         
